7b29bfad0b800bba142a6af493ddd212360f594c
sachin <sachncs@gmail.com>
2026-09-04 08:40:04 +0530

    refactor(catalogstore): rename rqvaestore.py -> semanticids.py; class->semanticids; registry 'semanticids'

--- braid/catalogstore/__init__.py
+++ braid/catalogstore/__init__.py
@@ -3,6 +3,6 @@
 from braid.catalogstore.matmulinmem import matmulinmem
 from braid.catalogstore.matmulint4awq import matmulint4awq
 from braid.catalogstore.faissivfstore import faissivfstore
-from braid.catalogstore.rqvaestore import rqvaestore
+from braid.catalogstore.semanticids import semanticids
 
-__all__ = ["matmulinmem", "matmulint4awq", "faissivfstore", "rqvaestore"]
+__all__ = ["matmulinmem", "matmulint4awq", "faissivfstore", "semanticids"]
